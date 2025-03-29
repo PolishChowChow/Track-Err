acd49510577c4b87f37f524156591c0c85314d76
PolishChowChow <polishchowchow@gmail.com>
2025-03-29 10:11:27 +0100

    feat: added menu as a Footer of the app

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Text, View } from "react-native"
+import Footer from "./components/Footer/Footer";
 export default function RootLayout() {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
         <Stack />
+        <Footer />
     </QueryClientProvider>
   );
 }
